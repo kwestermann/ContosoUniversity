9a4b58cd0fcac00eb4e2cf5de928e85d2ba5370e
kwestermann <kriswestermann@yahoo.com>
2015-06-01 14:40:52 -0400

    Completed: Creating a More Complexed Data Model Skipped: Deploying in Azure

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -11,19 +11,28 @@ namespace ContosoUniversity.DAL
     {
         // The name of the connection string (which you'll add to the Web.config file later) 
         // is passed in to the constructor.
-        public SchoolContext() : base("SchoolContext")
-        {
-        }
+        //public SchoolContext() : base("SchoolContext")
+        //{
+        //}
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             // The modelBuilder.Conventions.Remove statement in the OnModelCreating method 
             // prevents table names from being pluralized. 
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                    .MapRightKey("InstructorID")
+                    .ToTable("CourseInstructor"));
         }
     }
 }
