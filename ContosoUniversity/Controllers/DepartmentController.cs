5a7163d6d9973b42c9e0310bc9eb5a0bdae61768
kwestermann <kriswestermann@yahoo.com>
2015-06-02 16:25:43 -0400

    Completed: Async and Stored Procedures Next: Handling Concurrency

---
+++ ContosoUniversity/Controllers/DepartmentController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // The method is marked with the async keyword, which tells the compiler to generate callbacks for parts of the method body and to automatically create the Task<ActionResult> object that is returned.
+        // The return type was changed from ActionResult to Task<ActionResult> . The Task<T> type represents ongoing work with a result of type T.
+        // The await keyword was applied to the web service call. When the compiler sees this keyword, behind the scenes it splits the method into two parts. The first 
+        //   part ends with the operation that is started asynchronously. The second part is put into a callback method that is called when the operation completes.
+        // The asynchronous version of the ToList extension method was called.
+
+        // GET: Department
+        public async Task<ActionResult> Index()
+        {
+            var departments = db.Departments.Include(d => d.Administrator);
+            return View(await departments.ToListAsync());
+        }
+
+        // GET: Department/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await db.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName");
+            return View();
+        }
+
+        // POST: Department/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Departments.Add(department);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await db.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "DepartmentID,Name,Budget,StartDate,InstructorID")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(department).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "ID", "LastName", department.InstructorID);
+            return View(department);
+        }
+
+        // GET: Department/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await db.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Department department = await db.Departments.FindAsync(id);
+            db.Departments.Remove(department);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -33,6 +33,7 @@ namespace ContosoUniversity.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
